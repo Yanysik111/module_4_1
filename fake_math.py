fb2eda1fe6fa10a178966f94ce713a76b3c122a3
Yanysik111 <yanyisik@yandex.ru>
2024-06-14 12:05:21 +0300

    Update fake_math.py

--- fake_math.py
+++ fake_math.py
@@ -1,6 +1,5 @@
-from math import inf
 def divide(first, second):
     if second == 0:
-        return inf
+        return 'Ошибка'
     else:
-        return first/second
+        return first / second
